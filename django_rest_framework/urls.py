939ce658ea7ad98091d576ea8d7bdd73aff4f621
照井拓朗 <>
2022-04-28 16:10:45 +0900

    プロジェクト名変更

--- django_rest_framework/urls.py
+++ django_rest_framework/urls.py
@@ -1,4 +1,4 @@
-"""django_rest_framework_test URL Configuration
+"""django_rest_framework URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
