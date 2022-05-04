85c2e3c0a8c88927d98b87c82818ce195d5623bd
照井拓朗 <>
2022-05-04 20:51:34 +0900

    フィルパス指定

--- django_rest_framework/settings.py
+++ django_rest_framework/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -147,3 +148,6 @@ REST_FRAMEWORK = {
     'NON_FIELD_ERRORS_KEY': 'detail',
     'TEST_REQUEST_DEFAULT_FORMAT': 'json',
 }
+
+IMAGE_ROOT = os.path.join(BASE_DIR, 'images')
+IMAGE_URL = './images/'
